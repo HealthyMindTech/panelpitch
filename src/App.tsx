07d72b6a56f5a21a0e95f2104ad3aa495dfa84ec
Paula Petcu <petcu.paula@gmail.com>
2023-05-18 22:58:32 +0200

    Add top bar

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Advisors } from './components/advisors';
 import { PitchForm } from './components/pitch';
 import { ADVISORS, AdvisorStatus } from './models/advisor';
 import './App.scss';
+import { Button } from 'react-bootstrap';
 
 const INITIAL_ADVISOR_STATUS: Record<string, AdvisorStatus> = Object.fromEntries(ADVISORS.map<[string, AdvisorStatus]>((advisor) => {
   return [advisor.id, {
@@ -19,6 +20,10 @@ function App() {
   return (
     <div className="App">
       <Container className="min-h-screen flex flex-col">
+        <div className="bg-white text-black p-2 flex justify-between items-center">
+          <div className="text-lg font-bold justify-center text-center justify-center w-full">HACKATHON ADVISORY BOARD</div>
+          <Button variant="primary" size="sm" className="text-sm">info</Button>
+        </div>
         <Advisors advisorStatus={advisorStatus} />
         <PitchForm advisors={ADVISORS} setAdvisorStatus={setAdvisorStatus} />
       </Container>
