c108c5088dec848d1a83db61ced295dbeda7265f
Martin Basterrechea <mbs@reshapebiotech.com>
2023-05-18 22:55:58 +0200

    chat looks nicer

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -55,27 +55,22 @@ const testMessages = [
 
 const Chat = ({ chatParticipants } : { chatParticipants: Advisor[] }): JSX.Element => {
     return (
-        <div className="w-100 flex flex-col h-full">
+        <div className="w-100 flex flex-col h-full space-y-4 px-2 pb-2">
             <div className="flex justify-center">
-                <div className="font-semibold">Chat title</div>
+                <div className="font-semibold mr-1">Chat with experts: </div>
                 <div>
-                    Participant names:
-                    {chatParticipants.map((participant) => {
-                        return (
-                            <span key={participant.name}>{participant.name}</span>
-                        );
-                    })}
+                    {chatParticipants.map(advisor => advisor.name).join(", ") + "."}
 
                 </div>
             </div>
             {/* Chat messages area */}
             {/* If its from the advisors, put on left, if its user, right. */}
             <div className="flex justify-center w-100 flex-grow overflow-y-auto">
-                <div className="bg-gray-100 rounded-xl p-2 px-4 w-100 overflow-y-auto h-full">
+                <div className="bg-gray-100 rounded-xl p-2 px-4 w-100 overflow-y-auto h-full border-x border-y border-gray-300">
                     {testMessages.map((message, index) => {
                         if (message.user === null) {
                             return (
-                                <div className="flex justify-end" key={message.text + index}>
+                                <div className="flex justify-end mb-2" key={message.text + index}>
                                     <div className="bg-blue-500 rounded-xl text-white p-2 px-4">
                                         {message.text}
                                     </div>
@@ -83,7 +78,11 @@ const Chat = ({ chatParticipants } : { chatParticipants: Advisor[] }): JSX.Eleme
                             );
                         } else {
                             return (
-                                <div className="flex justify-start" key={message.text + index}>
+                                <div className="flex justify-start mb-2" key={message.text + index}>
+                                    <div className="flex flex-col justify-center items-center mr-2">
+                                        <img src={message.user.imageUrl} alt={message.user.name} className="rounded-full w-10 h-10" />
+                                        <div className="text-xs align-center font-semibold" >{message.user.name}</div>
+                                    </div>
                                     <div className="bg-gray-200 rounded-xl p-2 px-4">
                                         {message.text}
                                     </div>
@@ -95,10 +94,10 @@ const Chat = ({ chatParticipants } : { chatParticipants: Advisor[] }): JSX.Eleme
             </div>
             {/* Chat input area */}
             <div className="flex justify-center w-100">
-                <div className="bg-gray-100 rounded-xl p-2 px-4 w-100 mt-2">
-                    <div className="flex justify-between">
-                        <input type="text" className="w-full rounded-xl p-2 px-4" />
-                        <button className="bg-blue-500 rounded-xl text-white p-2 pt-0"><span className="material-symbols-outlined relative top-1.5 mr-1">send</span>Send</button>
+                <div className="w-100 mt-2">
+                    <div className="flex justify-between border-x border-y border-gray-300 rounded-xl overflow-clip">
+                        <input type="text" className="w-full p-2 px-4" />
+                        <button className="bg-blue-500 text-white p-2 pt-0"><span className="material-symbols-outlined relative top-1.5 mr-1">send</span>Send</button>
                     </div>
                 </div>
             </div>
