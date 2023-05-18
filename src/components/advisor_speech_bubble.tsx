b6b5ee15875320cded1ca50ade828258dfcb49c1
Troels Nielsen <bn.troels@gmail.com>
2023-05-18 22:40:41 +0200

    Loading speech bubble

--- src/components/advisor_speech_bubble.tsx
+++ src/components/advisor_speech_bubble.tsx
@@ -1,23 +1,32 @@
 import './advisor_speech_bubble.css';
 import { Chat } from './chat';
 import { Advisor } from '../models/advisor';
-
+import Spinner from 'react-bootstrap/Spinner';
 
 interface AdvisorSpeechBubbleProps {
     text: string;
     expanded: boolean;
     closeChat: () => void;
     chatParticipants: Advisor[];
+    status: 'ready' | 'loading' | 'error';
 }
 
-const AdvisorSpeechBubble = ({ text, expanded, closeChat, chatParticipants }: AdvisorSpeechBubbleProps): JSX.Element => {
+const expandedText = (text: string, status: 'ready' | 'loading' | 'error'): JSX.Element => {
+    if (status === 'loading') {
+        return <Spinner animation="border" role="status">
+            <span className="visually-hidden">Loading...</span>
+        </Spinner>
+    }
+    return <>{text}</>;
+}
+const AdvisorSpeechBubble = ({ status, text, expanded, closeChat, chatParticipants }: AdvisorSpeechBubbleProps): JSX.Element => {
     return (
         <div className="h-full" style={{maxHeight: expanded ? '370px' : '182px'}}>
             <div
                 className={`border-x border-y rounded-2xl border-gray-300 bg-white mx-2 p-2 AdvisorSpeechBubble h-full z-20 ${!expanded && 'overflow-y-auto'}`}
             >   
                 {expanded}
-                {!expanded && text}
+                {!expanded && expandedText(text, status)}
                 {expanded && <Chat chatParticipants={chatParticipants} />}
             </div>
         </div>
--- src/components/advisors.tsx
+++ src/components/advisors.tsx
@@ -35,10 +35,10 @@ const Advisors = ({advisorStatus}: {advisorStatus: Record<string, AdvisorStatus>
             </div>
             {/* if collapsed: 1x5, if expanded, 2x10 */}
             <div className={expanded === -1 ? "col-span-3" : expanded === 0 ? "col-start-2 row-start-1 row-span-2 col-span-6" : "hidden"}>
-                <AdvisorSpeechBubble text={advisorStatus[UX.id].message} expanded={expanded === 0} closeChat={() => {setExpanded(-1)}} chatParticipants={chatParticipants}/>
+                <AdvisorSpeechBubble status={advisorStatus[UX.id].status} text={advisorStatus[UX.id].message} expanded={expanded === 0} closeChat={() => {setExpanded(-1)}} chatParticipants={chatParticipants}/>
             </div>
             <div className={expanded === -1 ? "col-span-3" : expanded === 1 ? "col-start-2 row-start-1 row-span-2 col-span-6" : "hidden" }>
-                <AdvisorSpeechBubble text={advisorStatus[Pitch.id].message} expanded={expanded === 1} closeChat={() => {setExpanded(-1)}} chatParticipants={chatParticipants}/>
+                <AdvisorSpeechBubble status={advisorStatus[Pitch.id].status} text={advisorStatus[Pitch.id].message} expanded={expanded === 1} closeChat={() => {setExpanded(-1)}} chatParticipants={chatParticipants}/>
             </div>
             <div>
                 <AdvisorCard advisor={Pitch} side='right' onChatClick={handleChatClick} inChat={chatParticipants.indexOf(Pitch) !== -1} chatOpen={expanded !== -1}/>
@@ -47,10 +47,10 @@ const Advisors = ({advisorStatus}: {advisorStatus: Record<string, AdvisorStatus>
                 <AdvisorCard advisor={Dev} side='left' onChatClick={handleChatClick} inChat={chatParticipants.indexOf(Dev) !== -1} chatOpen={expanded !== -1}/>
             </div>
             <div className={expanded === -1 ? "col-span-3" : expanded === 2 ? "col-start-2 row-start-1 row-span-2 col-span-6" : "hidden" }>
-                <AdvisorSpeechBubble text={advisorStatus[Dev.id].message} expanded={expanded === 2} closeChat={() => {setExpanded(-1)}} chatParticipants={chatParticipants}/>
+                <AdvisorSpeechBubble status={advisorStatus[Dev.id].status} text={advisorStatus[Dev.id].message} expanded={expanded === 2} closeChat={() => {setExpanded(-1)}} chatParticipants={chatParticipants}/>
             </div>
             <div className={expanded === -1 ? "col-span-3" : expanded === 3 ? "col-start-2 row-start-1 row-span-2 col-span-6" : "hidden" }>
-                <AdvisorSpeechBubble text={advisorStatus[Market.id].message} expanded={expanded === 3} closeChat={() => {setExpanded(-1)}} chatParticipants={chatParticipants}/>
+                <AdvisorSpeechBubble status={advisorStatus[Market.id].status} text={advisorStatus[Market.id].message} expanded={expanded === 3} closeChat={() => {setExpanded(-1)}} chatParticipants={chatParticipants}/>
             </div>
             <div>
                 <AdvisorCard advisor={Market} side='right' onChatClick={handleChatClick} inChat={chatParticipants.indexOf(Market) !== -1} chatOpen={expanded !== -1}/>
