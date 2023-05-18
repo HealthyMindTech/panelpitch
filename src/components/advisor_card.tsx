45091ce26bd48fe6d3575931554f2ac64f6d5a7a
Paula Petcu <petcu.paula@gmail.com>
2023-05-18 20:23:51 +0200

    Put chat button below so it looks reasonable

--- src/components/advisor_card.tsx
+++ src/components/advisor_card.tsx
@@ -3,19 +3,21 @@ import { Advisor } from '../models/advisor';
 
 const AdvisorCard = ({advisor, side, onChatClick} : { advisor: Advisor, side: 'left' | 'right', onChatClick: () => void}) : JSX.Element => {
     return (
-        <div className='p-2 rounded-2xl flex flex-col' style={{background: advisor.bgColor, width: '100px', minWidth: '100px'}}>
-            <div className={`flex ${side === 'left' ? 'justify-end' : ''}`}>
-                <div 
-                    className="border-x border-y rounded-lg p-0.5 bg-gray-100 mb-1"
-                    onClick={onChatClick}   
-                >
-                    Chat
-                </div>
-            </div>
+        <div className='p-2 rounded-2xl flex flex-col' style={{background: advisor.bgColor, width: '120px', minWidth: '120px'}}>
+            
             <div className="h-64">
-                <img src={advisor.imageUrl} alt={advisor.name} className="h-100 aspect-auto  mx-auto" style={{width: 64}}/>
+                <img src={advisor.imageUrl} alt={advisor.name} className="h-100 aspect-auto mx-auto" style={{width: 64}}/>
             </div>
             <div className="font-semibold text-center">{advisor.name}</div>
+            <div className={`flex ${side === 'left' ? 'justify-end' : ''}`}>
+                <div 
+                    className="border-x border-y rounded-lg p-0.5 bg-gray-100 mb-1 text-sm"
+                    onClick={onChatClick}   
+                >
+                    <span className="material-symbols-outlined relative top-1 mr-1 text-sm">chat</span>
+                    Ask to chat
+                </div>
+            </div>
         </div>
     );
 }
